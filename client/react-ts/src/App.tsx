e208cdb3e8a174f9bcc2fb6984e0e92a15e2bc2f
Dmitry Jum <xfiler@ymail.com>
2024-07-22 00:00:52 -0700

    adjusts styles to center the list

--- client/react-ts/src/App.tsx
+++ client/react-ts/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ApolloProvider, client } from './apolloClient';
 import Messages from './Messages';
+import styles from './App.module.scss';
 
 const App: React.FC = () => (
   // Wrap the application with ApolloPrivder and pass the Apollo Client instance
   <ApolloProvider client={client}>
-    <div className='app-wrapper'>
+    <div className={styles.appWrapper}>
       <h1>Real-Time Message Board</h1>
       <h2>GraphQL Subscriptions with React</h2>
       <Messages /> {/* Render the Messages component */}
